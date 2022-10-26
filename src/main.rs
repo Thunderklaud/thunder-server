9cdfc6a9b5e85e63b959df5f3ee663d1fac2db14
binsky <timo@binsky.org>
2022-10-26 20:42:40 +0200

    add initial rocket framework with a basic settings implementation

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate rocket;
+
+mod settings;
+
+use once_cell::sync::OnceCell;
+use rocket::{Request, Route, route};
+use rocket::http::{Method::{Get, Patch, Post, Put, Delete}};
+
+static SETTINGS: OnceCell<settings::Settings> = OnceCell::new();
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    let settings = settings::Settings::new().unwrap();
+    SETTINGS.set(settings).unwrap();
+
+    // Print out our settings
+    println!("{:?}", SETTINGS);
+    /*
+    rocket::build()
+        .mount("/v1/user", vec![
+            Route::new(Post, "/login", controller::user::login),
+            Route::new(Post, "/logout", controller::user::logout),
+            Route::new(Post, "/registration", controller::user::register),
+        ])
+        .mount("/v1/data", vec![
+            Route::new(Get, "/test", controller::file::test),
+            Route::new(Post, "/file", controller::file::create),
+            Route::new(Put, "/file", controller::file::upload),
+            Route::new(Get, "/file", controller::file::download),
+        ])
+        .launch().await;*/
+}
